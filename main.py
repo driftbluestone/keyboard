ce7ab25fa18f31a2ae46be9e0bfd1f35ce5d2614
driftbluestone <driftbluestone@gmail.com>
2026-06-21 13:05:10 -0700

    now uses pageui

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ class Kb(commands.Cog):
     
     @app_commands.command(name="keyboard", description="haha funny keyboard")
     async def keyboard(self, interaction: discord.Interaction):
-        await interaction.response.send_message(view=Keyboard(interaction, ""))
+        await interaction.response.send_message(view=Keyboard(""))
 
-class Keyboard(gui.MenuGUI):
-    def __init__(self, interaction: discord.Interaction, data_transfer: str, page: int = 1):
+class Keyboard(gui.PageUI):
+    def __init__(self, data_transfer: str, page: int = 1):
         keys = sorted(list(set("ABCDEFGHIJKJMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-=_+[]{};':\"\\,.<>/?`~!@#$%^&*()")))
-        super().__init__(interaction=interaction, element_count=len(keys), data_transfer=data_transfer, text=data_transfer, page=page)
+        super().__init__(element_count=len(keys), data_transfer=data_transfer, text=data_transfer, page=page)
         keys = keys[((self.page-1)*10):(self.page*10)]
         for key in keys:
             button = discord.ui.Button(label = key, style=discord.ButtonStyle.blurple, custom_id=key)
@@ -34,15 +34,15 @@ class Keyboard(gui.MenuGUI):
     async def callback(self, interaction: discord.Interaction):
         self.data_transfer += interaction.data["custom_id"]
         await interaction.response.defer(ephemeral=True, thinking=False)
-        await self.interaction.edit_original_response(content=self.data_transfer)
+        await interaction.message.edit(content=self.data_transfer)
 
     async def backspace(self, interaction: discord.Interaction):
         self.data_transfer = self.data_transfer[:-1]
         await interaction.response.defer(ephemeral=True, thinking=False)
-        await self.interaction.edit_original_response(content=self.data_transfer)
+        await interaction.message.edit(content=self.data_transfer)
 
     async def space(self, interaction: discord.Interaction):
         self.data_transfer += " "
         await interaction.response.defer(ephemeral=True, thinking=False)
-        await self.interaction.edit_original_response(content=self.data_transfer)
+        await interaction.message.edit(content=self.data_transfer)
 
